c51b2a85a728aab9b8e32d1de7fbbfeb0b5ddce7
FUJITSU <fiorena070307@gmail.com>
2026-04-26 21:32:23 +0700

    Pertemuan 7

--- app/Http/DosenController.php
+++ app/Http/DosenController.php
@@ -1,15 +1,21 @@
 <?php
-//package
+// package
 namespace App\Http\Controllers;
-//import
+// import
 use Illuminate\Http\Request;
 
 class DosenController extends Controller
 {
+    //
+    public function index()
+    {
+        return "<h1> Halo ini adalah method index, dalam controller DosenController </h1> di tutorial laravel www.malasngoding.com";
+    }
 
-public
-
-public function biodata() {
-        	$nama = "Diki Alfarabi Hadi";
-    	return view('biodata',['nama' => $nama]);
+    public function biodata(){
+        $nama = "Diki Alfarabi Hadi";
+        $umur = 31;
+        $pelajaran = ["Algoritma & Pemrograman", "Kalkulus", "Pemrograman Web"];
+    	return view('biodata',['nama' => $nama, 'umur' => $umur, 'matkul' => $pelajaran]);
+    }
 }
